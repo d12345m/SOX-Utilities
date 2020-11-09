50cab86dabe9b3b106e4890e4e52257dad89ba67
Derek Miller <d12345m@gmail.com>
2020-11-08 19:26:46 -0500

    Script now takes filepath of input directory as command line argument.

--- SOX.sh
+++ SOX.sh
@@ -1,13 +1,29 @@
 #! /bin/bash
 
-orig_dir="/users/derekmiller/Desktop/Test"
+while getopts "i:" opt; do
+  case ${opt} in
+    i ) 
+    INPUTDIRECTORY=$OPTARG
+      ;;
+    \? ) echo "Usage: cmd [-i]"
+      ;;
+  esac
+done
 
-cd "/users/derekmiller/Desktop"  
-cp -r $orig_dir "Temp"
+if [[ -z "$INPUTDIRECTORY" ]]
+then
+    echo "Input file path is required."
+    exit 1
+fi 
+
+DESKTOP=$HOME/Desktop
+cd $DESKTOP  
+
+cp -r $INPUTDIRECTORY "Temp"
 
 # Strip silence from end of files.
 # Store temp copy of output files.
-cd "/users/derekmiller/Desktop/Temp"
+cd "$DESKTOP/Temp"
 for D in */ ;
 do
     (cd "$D"
@@ -20,14 +36,14 @@ do
 done;
 
 # Add fade in and fade out to avoid popping.
-cd "/users/derekmiller/Desktop/Temp"
-mkdir "/users/derekmiller/Desktop/Temp2"
+cd "$DESKTOP/Temp"
+mkdir "$DESKTOP/Temp2"
 for D in */ ;
 do
     (cd "$D"
-    mkdir "/users/derekmiller/Desktop/Temp2/$(basename -- "$D")"
+    mkdir "$DESKTOP/Temp2/$(basename -- "$D")"
     find . -type f -name "*.wav" | while read filename; do
-        sox "$filename" "/users/derekmiller/Desktop/Temp2/"$(basename -- "$D")"/$(basename -- "$filename")" fade 0.001 0 0.001;
+        sox "$filename" "$DESKTOP/Temp2/"$(basename -- "$D")"/$(basename -- "$filename")" fade 0.001 0 0.001;
     done;)
 done;
 
@@ -35,26 +51,26 @@ done;
 
 # Process stereo files.
 # Normalize.
-cd "/users/derekmiller/Desktop/Temp2"
-mkdir "/users/derekmiller/Desktop/StereoTemp"
+cd "$DESKTOP/Temp2"
+mkdir "$DESKTOP/StereoTemp"
 for D in */ ;
 do
     (cd "$D"
-    mkdir "/users/derekmiller/Desktop/StereoTemp/$(basename -- "$D")"
+    mkdir "$DESKTOP/StereoTemp/$(basename -- "$D")"
     find . -type f -name "*.wav" | while read filename; do
-	   sox "$filename" "/users/derekmiller/Desktop/StereoTemp/"$(basename -- "$D")"/$(basename -- "$filename")" norm -0.1;
+	   sox "$filename" "$DESKTOP/StereoTemp/"$(basename -- "$D")"/$(basename -- "$filename")" norm -0.1;
     done;)
 done;
 
 # Change bit rate and bit depth.
-cd "/users/derekmiller/Desktop/StereoTemp"
-mkdir "/users/derekmiller/Desktop/Stereo"
+cd "$DESKTOP/StereoTemp"
+mkdir "$DESKTOP/Stereo"
 for D in */ ;
 do
     (cd "$D"
-    mkdir "/users/derekmiller/Desktop/Stereo/$(basename -- "$D")"
+    mkdir "$DESKTOP/Stereo/$(basename -- "$D")"
     find . -type f -name "*.wav" | while read filename; do
-	    sox "$filename" -b 16 -r 44100 "/users/derekmiller/Desktop/Stereo/"$(basename -- "$D")"/$(basename -- "$filename")";
+	    sox "$filename" -b 16 -r 44100 "$DESKTOP/Stereo/"$(basename -- "$D")"/$(basename -- "$filename")";
     done;)
 done;
 
@@ -62,48 +78,48 @@ done;
 
 # Process mono files.
 # Convert to mono.
-cd "/users/derekmiller/Desktop/Temp2"
-mkdir "/users/derekmiller/Desktop/MonoTemp"
+cd "$DESKTOP/Temp2"
+mkdir "$DESKTOP/MonoTemp"
 for D in */ ;
 do
     (cd "$D"
-    mkdir "/users/derekmiller/Desktop/MonoTemp/$(basename -- "$D")"
+    mkdir "$DESKTOP/MonoTemp/$(basename -- "$D")"
     find . -type f -name "*.wav" | while read filename; do
-        sox -v 0.96 "$filename" "/users/derekmiller/Desktop/MonoTemp/"$(basename -- "$D")"/$(basename -- "$filename")" remix 1-2;
+        sox -v 0.96 "$filename" "$DESKTOP/MonoTemp/"$(basename -- "$D")"/$(basename -- "$filename")" remix 1-2;
     done;)
 done;
 
 # Normalize.
-cd "/users/derekmiller/Desktop/MonoTemp"
-mkdir "/users/derekmiller/Desktop/MonoTemp2"
+cd "$DESKTOP/MonoTemp"
+mkdir "$DESKTOP/MonoTemp2"
 for D in */ ;
 do
     (cd "$D"
-    mkdir "/users/derekmiller/Desktop/MonoTemp2/$(basename -- "$D")"
+    mkdir "$DESKTOP/MonoTemp2/$(basename -- "$D")"
     find . -type f -name "*.wav" | while read filename; do
-	   sox "$filename" "/users/derekmiller/Desktop/MonoTemp2/"$(basename -- "$D")"/$(basename -- "$filename")" norm -0.1;
+	   sox "$filename" "$DESKTOP/MonoTemp2/"$(basename -- "$D")"/$(basename -- "$filename")" norm -0.1;
     done;)
 done;
 
 # Change bit rate and bit depth.
-cd "/users/derekmiller/Desktop/MonoTemp2"
-mkdir "/users/derekmiller/Desktop/Mono"
+cd "$DESKTOP/MonoTemp2"
+mkdir "$DESKTOP/Mono"
 for D in */ ;
 do
     (cd "$D"
-    mkdir "/users/derekmiller/Desktop/Mono/$(basename -- "$D")"
+    mkdir "$DESKTOP/Mono/$(basename -- "$D")"
     find . -type f -name "*.wav" | while read filename; do
-	    sox "$filename" -b 16 -r 44100 "/users/derekmiller/Desktop/Mono/"$(basename -- "$D")"/$(basename -- "$filename")";
+	    sox "$filename" -b 16 -r 44100 "$DESKTOP/Mono/"$(basename -- "$D")"/$(basename -- "$filename")";
     done;)
 done;
 
 #------------------------------------------------------------------------------
 
 # Clean up.
-rm -r "/users/derekmiller/Desktop/Temp"
-rm -r "/users/derekmiller/Desktop/Temp2"
-rm -r "/users/derekmiller/Desktop/StereoTemp"
-rm -r "/users/derekmiller/Desktop/MonoTemp"
-rm -r "/users/derekmiller/Desktop/MonoTemp2"
+rm -r "$DESKTOP/Temp"
+rm -r "$DESKTOP/Temp2"
+rm -r "$DESKTOP/StereoTemp"
+rm -r "$DESKTOP/MonoTemp"
+rm -r "$DESKTOP/MonoTemp2"
 
 # end checks - number of files in the original directory should match Stereo and Mono directories. All files should be 16-bit. All files in Stereo directory should be stereo. All files in Mono directory should be mono.
